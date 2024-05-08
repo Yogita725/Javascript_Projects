00961ac20f4af4185bc49c0ba02508a3a191e242
Yogita725 <vermayogita725@gmail.com>
2024-05-08 22:58:03 +0530

    Practicing sessionStorage

---
+++ Practice sessionStorage/app.js
@@ -0,0 +1,26 @@
+
+
+let incrementBtn = document.querySelector('#incrementBtn');
+let decrementBtn = document.querySelector('#decrementBtn');
+let count = document.querySelector('#count');
+
+incrementBtn.addEventListener('click',()=>{
+    let counter = parseInt(sessionStorage.getItem('counter')) || 0;
+    counter = counter +1;
+    sessionStorage.setItem('counter',counter);
+    updateSession();
+})
+
+decrementBtn.addEventListener('click',()=>{
+    let counter = parseInt(sessionStorage.getItem('counter')) || 0;
+    counter = counter - 1;
+    sessionStorage.setItem('counter',counter);
+    updateSession();
+})
+
+function updateSession(){
+    let counter = parseInt(sessionStorage.getItem('counter')) || 0;
+    count.innerText = counter;
+}
+
+updateSession();
